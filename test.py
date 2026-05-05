258d8d5663ca2d0771c4b6fd9387bd3ad4d09959
dwijit <dasaridwijit90@gmail.com>
2026-05-05 20:59:10 +0530

    Optimized the scheduler with Threadpool & added the testurls

--- test.py
+++ test.py
@@ -1,16 +1,38 @@
-def outer_func(name):
-    def decorator(func):
-        def process():
-            print("I will do some process here")
-            func(name)
-            print("This process is done")
-        return process
-    return decorator
-
-@outer_func('Rahul')
-def hello(name):
-    print("Hello world" , name)
-hello()
-
-# hello = outer_func(hello)
-# hello()
+import requests
+d = {
+  "test_urls": [
+    "https://example.com",
+    "https://www.google.com",
+    "https://www.wikipedia.org",
+    "https://httpbin.org/get",
+    "https://jsonplaceholder.typicode.com/posts",
+    
+    "https://httpbin.org/delay/1",
+    "https://httpbin.org/delay/2",
+    "https://httpbin.org/delay/3",
+    "https://httpbin.org/delay/5",
+    
+    "https://httpbin.org/bytes/1024",
+    "https://httpbin.org/bytes/20480",
+    "https://jsonplaceholder.typicode.com/photos",
+    
+    "https://httpbin.org/post",
+    "https://httpbin.org/put",
+    "https://httpbin.org/delete",
+    
+    "https://httpbin.org/status/200",
+    "https://httpbin.org/status/404",
+    "https://httpbin.org/status/500",
+    
+    "https://api.github.com",
+    "https://catfact.ninja/fact"
+  ]
+}
+for url in d["test_urls"]:
+    data = {
+    "url_link": url,
+    "url_name": "string",
+    "user_id": 1
+    }
+    requests.post(url="http://localhost:8000/urls",json=data)
+print("Done")
--- url_scheduler/scheduler.py
+++ url_scheduler/scheduler.py
@@ -1,7 +1,20 @@
 import time
 from db.queries import DBManager
 from url_scheduler.healthservice import check_status
+from concurrent.futures import ThreadPoolExecutor
+import time
 dbmanger = DBManager()
+
+def process_url(url_id:int):
+    s = dbmanger.get_session()
+    session = next(s)
+
+    try:
+        dbmanger.check_and_updatestatusdb(url_id,session)
+    finally:
+        session.close()
+
+
 def scheduler_data():
     print("Started the scheduler.....")
 
@@ -12,11 +25,19 @@ def scheduler_data():
 
             urlids = dbmanger.getallurlsids(session)
 
-            for ids in urlids:
-                try:
-                    dbmanger.check_and_updatestatusdb(ids, session)
-                except Exception as e:
-                    print("Error checking URL:", e)
+            #for ids in urlids:
+            try:
+                    #the below is the function that take every ids and run and update their status in db
+                    #for speedint up this we use ThreadPoolExecutor 
+                    #for network calls multiple threads GIL is realsed if waiting time is there so little parllel/concurent execution
+                start = time.time()
+                with ThreadPoolExecutor(max_workers=5) as executor:
+                    executor.map(process_url,urlids)
+                    # dbmanger.check_and_updatestatusdb(ids, session)
+                end = time.time()
+                print(f"Processsed {len(urlids)} in {end-start:.2f} sec...")
+            except Exception as e:
+                print("Error checking URL:", e)
 
         except Exception as e:
             print("Scheduler loop error:", e)
